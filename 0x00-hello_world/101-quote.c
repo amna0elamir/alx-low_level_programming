d57c3a19ab40f375b764f2f5bdf64f7de2466d56
amna0elamir <amna00elamir@gmail.com>
2023-09-07 16:49:55 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Return: Always 1 (Success)
@@ -7,7 +6,7 @@
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	       	59);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 	return (1);
 }
