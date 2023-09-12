9e97c44979b57a329032cf017b77d9154a759b84
amna0elamir <amna00elamir@gmail.com>
2023-09-12 16:31:14 -0700

    C programming_functions_nestedloops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,11 @@
  * print_alphabet_x10 - function to  print the alphabet 10 times
  *
 */
-
 void print_alphabet_x10(void)
 {
 	int r,c;
 
-	for (r= 0; r<= 9; r++)
+	for (r = 0; r <= 9; r++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
