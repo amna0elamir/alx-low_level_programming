4e7a9ba6dd547daeee79fb6dc4ae11f4a381d6ac
amna0elamir <amna00elamir@gmail.com>
2023-09-21 18:47:53 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,7 +6,6 @@ int main(void)
   int a[5];
   int *p;
 
-<<<<<<< HEAD
   a[2] = 1024;
   p = &n;
   /*
@@ -21,20 +20,4 @@ int main(void)
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
-=======
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
- 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
->>>>>>> 383183b30ae9685f1b36b693c0f4828407df7df7
 }
