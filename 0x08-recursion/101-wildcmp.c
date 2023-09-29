e2e00d05a979b771347daf7088677935dd0aa43f
amna0elamir <amna00elamir@gmail.com>
2023-09-29 02:35:24 +0200

    0x08. C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * wildcmp - function that compares two strings 
+ * wildcmp - function that compares two strin
  * @s1: input string
  * @s2:input str
  *
@@ -11,11 +11,12 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s2 == '*') {
-		while (*s1 != '\0') {
-			if (wildcmp(s1, s2 + 1)) {
+	if (*s2 == '*')
+	{
+		while (*s1 != '\0')
+		{
+			if (wildcmp(s1, s2 + 1))
 				return (1);
-			}
 			s1++;
 		}
 		return (wildcmp(s1, s2 + 1));
