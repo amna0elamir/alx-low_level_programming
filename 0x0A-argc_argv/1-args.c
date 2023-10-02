812392da468a0eef90fe3d7c6fee32e929225f2c
amna0elamir <amna00elamir@gmail.com>
2023-10-03 00:32:31 +0200

    0x0A. C - Argc_Argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,12 @@
  * @argc: arg count
  * @argv: arg vector
  *
- * Result: 0
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
 	(void)argv;
 	if (argc > 0)
-		printf("%d\n",argc - 1);
+		printf("%d\n", argc - 1);
 	return (0);
 }
