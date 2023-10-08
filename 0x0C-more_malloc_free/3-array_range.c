8fd93af8cf89408e255d70e75a4030c3e400332e
amna0elamir <amna00elamir@gmail.com>
2023-10-09 01:22:29 +0200

    0x0C. C - more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,17 +7,17 @@
  * @min: input num
  * @max:input num
  *
- * Return : pointer to array
+ * Return: pointer
  */
 int *array_range(int min, int max)
 {
-	int i = 0 , range;
-	int * create;
+	int i = 0, range;
+	int *create;
 
 	if (min > max)
 		return (NULL);
-	range = max - min ;
-	create = malloc((range + 1) * sizeof (int));
+	range = max - min;
+	create = malloc((range + 1) * (sizeof(int)));
 	if (create == NULL)
 		return (NULL);
 	for (; i <= range ; i++)
