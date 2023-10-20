9af8000ae1d046553e4d884e72338c2edc0136a1
amna0elamir <amna00elamir@gmail.com>
2023-10-20 03:39:39 +0200

    single_linked_list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,13 +2,13 @@
 
 /**
  * list_len - func for no  nodes of linked list
- * @h: head 
+ * @h: head
  * Return: number of nodes
  */
 size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
-	
+
 	while (h)
 	{
 		count++;
