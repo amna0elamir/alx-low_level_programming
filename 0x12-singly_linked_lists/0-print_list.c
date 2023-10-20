dd05cad0ffa37bc62749cd25fe93d64d8578a0d0
amna0elamir <amna00elamir@gmail.com>
2023-10-20 04:59:36 +0200

    single_linked_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,15 @@ size_t print_list(const list_t *h)
 {
 	unsigned int count = 0;
 
-	while (*h)
+	while (h)
 	{
 		printf("[%u] ", h->len);
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			count++;
+			printf("%s\n", h->str);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
